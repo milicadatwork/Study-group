b31afea4cadb92c6bd8b15a3e6e7989c8801c5c8
milicadatwork <milica.danicic@work.co>
2022-10-05 22:15:20 +0200

    add function printHelloName

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Study-group
+
+This program prints my name five times.
+
+
+To start the program type the following code in the terminal:
+``` 
+node hello.js
+```
---
+++ hello.js
@@ -0,0 +1,9 @@
+// Function to compute the product of p1 and p2
+function printHelloName(name) {
+    for (let i = 0; i < 5; i++) {
+        const element = 5[i];
+        console.log('Hello '+ name)  
+    }
+  return name;
+}
+printHelloName("Milica");
