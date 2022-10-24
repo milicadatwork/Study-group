d1076c4921067f3449adda924e70d10268085bf4
milicadatwork <milica.danicic@work.co>
2022-10-24 21:17:14 +0200

    PR fixes

--- hello.js
+++ hello.js
@@ -1,9 +1,7 @@
 // Function to compute the product of p1 and p2
 function printHelloName(name) {
     for (let i = 0; i < 5; i++) {
-        const element = 5[i];
         console.log('Hello '+ name)  
     }
-  return name;
 }
 printHelloName("Milica");
